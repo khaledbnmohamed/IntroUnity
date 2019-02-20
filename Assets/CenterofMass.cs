ede7a8f4f9055cb6685869b36067f585e2d946a4
khaledbnmohamed <34178030+khaledbnmohamed@users.noreply.github.com>
2019-02-20 02:46:18 +0200

    commit

---
+++ Assets/CenterofMass.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CenterofMass : MonoBehaviour
+{
+    public Vector3 com;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    private void Update()
+    {
+
+        com = this.transform.position;
+
+        rb.centerOfMass = com;
+    }
+
+}
